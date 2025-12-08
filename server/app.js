e584c0345246ffdf59103668e9895263a75f11e1
parsaDokhtMohammadi <parsadokhtmohammadi.work@gmail.com>
2025-12-08 11:06:24 +0330

    dockerfile modified

--- server/app.js
+++ server/app.js
@@ -4,8 +4,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/",(req, res)=>{
+    res.send("Hello World! lets test docker");
+})
 
-
-app.listen(4000,(req,res)=>{
+app.listen(4000,'0.0.0.0',(req,res)=>{
     console.log("server started http://localhost:4000")
 })
