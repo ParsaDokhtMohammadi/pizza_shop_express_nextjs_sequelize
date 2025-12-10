12f40bb10422e9db4c45df42bd9bbdf9fd5c16d0
parsaDokhtMohammadi <parsadokhtmohammadi.work@gmail.com>
2025-12-10 18:50:29 +0330

    login route added

--- server/modules/auth/auth.routes.js
+++ server/modules/auth/auth.routes.js
@@ -1,10 +1,11 @@
 import {Router} from "express";
 import { validate } from "express-validation";
-import {registerController} from "./auth.service.js";
+import {loginController, registerController} from "./auth.service.js";
 import { registerSchema } from "./auth.validation.js";
 const router = Router();
 
 router.post("/register",validate(registerSchema),registerController)
+router.post("/login",loginController)
 
 
 export default router
--- server/modules/auth/auth.service.js
+++ server/modules/auth/auth.service.js
@@ -3,6 +3,7 @@ import {nanoid} from "nanoid";
 import createHttpError from "http-errors";
 import {UserModel} from "../user/user.model.js";
 
+
 export const registerController = async (req , res , next)=>{
     try{
         const {full_name,email,password} = req.body;
@@ -18,7 +19,19 @@ export const registerController = async (req , res , next)=>{
 }
 export const loginController = async (req , res , next)=>{
     try{
-           
+        const {email,password} = req.body;
+        if(!password || !email)throw createHttpError(400, "email and password are required.");
+        const user = await UserModel.findOne({where : {email:email}})
+        if(!user) throw createHttpError(400, "invalid email or password");
+        const isPasswordValid = verifyPassword(password , user.password)
+        if(!isPasswordValid) throw createHttpError(400, "invalid email or password");
+        const token = generateToken({id : user.id , email : user.email})
+        res.cookie("planetPizza",token,{
+            maxage : 24 * 60 * 60 * 1000,
+            httpOnly : true,
+            signed : true,
+        })
+        res.status(200).json({ message : "login successful." });
     }catch(err){
         next(err)
     }
