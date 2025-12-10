63b27deaac19310f477f2737b97978ea2d92685c
parsaDokhtMohammadi <parsadokhtmohammadi.work@gmail.com>
2025-12-10 20:36:02 +0330

    auth tests updated  login route tests added

--- server/modules/user/user.test.js
+++ server/modules/user/user.test.js
@@ -1,20 +1,25 @@
-import request from "supertest"
-import {app} from "../../app.js";
-import {modelsInit} from "../../config/models.init.js";
-import {UserModel} from "./user.model.js";
+import request, { agent } from "supertest"
+import { app } from "../../app.js";
+import { modelsInit } from "../../config/models.init.js";
+import { UserModel } from "./user.model.js";
 import sequelize from "../../config/sequelize.config.js";
 
 
-describe("POST /api/auth/register", () => {
+describe("auth tests register,login,logout", () => {
+    let agent
     beforeAll(async () => {
         await modelsInit()
     })
+    beforeEach(() => {
+        agent = request.agent(app)
+    })
 
-    afterAll(async () => {1
+    afterAll(async () => {
+        1
         await sequelize.close();
     });
     test("should create user", async () => {
-        const response = await request(app).post('/api/auth/register').send({
+        const response = await agent.post('/api/auth/register').send({
             full_name: "John Doe",
             password: "Aa12345678",
             email: "johnDoe@gmail.com"
@@ -24,7 +29,7 @@ describe("POST /api/auth/register", () => {
         expect(response.body.message).toBe('user created successfully.')
     })
     test("email, password and full_name is not given", async () => {
-        const response = await request(app).post('/api/auth/register').send({
+        const response = await agent.post('/api/auth/register').send({
             full_name: "John Doe",
             password: "Aa12345678",
         })
@@ -33,7 +38,7 @@ describe("POST /api/auth/register", () => {
         expect(response.body.message).toBe('Email is required')
     })
     test("should say invalid email", async () => {
-        const response = await request(app).post('/api/auth/register').send({
+        const response = await agent.post('/api/auth/register').send({
             full_name: "John Doe",
             password: "Aa12345678",
             email: "johnDoel.com"
@@ -43,16 +48,17 @@ describe("POST /api/auth/register", () => {
         expect(response.body.message).toBe('Invalid email')
     })
     test("should say password must be between 8 and 32 chars", async () => {
-        const response = await request(app).post('/api/auth/register').send({
+        const response = await agent.post('/api/auth/register').send({
             full_name: "John Doe",
             password: "1234",
-            email: "johnDoel@gmail.com"})
+            email: "johnDoel@gmail.com"
+        })
         expect(response.status).toBe(400)
         expect(response.type).toBe('application/json')
         expect(response.body.message).toBe('Password must be between 8 and 32 charecters')
     })
     test("should say user already exists", async () => {
-        const response = await request(app).post('/api/auth/register').send({
+        const response = await agent.post('/api/auth/register').send({
             full_name: "John Doe",
             password: "Aa12345678",
             email: "johnDoe@gmail.com"
@@ -62,7 +68,53 @@ describe("POST /api/auth/register", () => {
         expect(response.body.message).toBe('user with this email already exists')
     })
     test("should find user in database", async () => {
-         const user = await UserModel.findOne({where : {email : "johnDoe@gmail.com"}})
-         expect(user).toBeTruthy()
+        const user = await UserModel.findOne({ where: { email: "johnDoe@gmail.com" } })
+        expect(user).toBeTruthy()
+    })
+    test("should login user And not allow second login", async () => {
+        const response = await agent.post('/api/auth/login').send({
+            email: "johnDoe@gmail.com",
+            password: "Aa12345678"
+        })
+        expect(response.status).toBe(200)
+        expect(response.type).toBe('application/json')
+        expect(response.body.message).toBe('login successful.')
+        expect(response.headers['set-cookie']).toBeDefined()
+        const responstAfterLogin = await agent.post("/api/auth/login").send({
+            email: "johnDoe@gmail.com",
+            password: "Aa12345678"
+        })
+        expect(responstAfterLogin.status).toBe(400)
+        expect(responstAfterLogin.type).toBe('application/json')
+        expect(responstAfterLogin.body.message).toBe("you are already logged in.")
+    })
+    test("should say password error", async () => {
+        const response = await agent.post("/api/auth/login").send({
+            email: "johnDoe@gmail.com",
+            password: "12345678"
+        })
+        expect(response.status).toBe(400)
+        expect(response.body.message).toBe("Password must contain at least one letter and one number")
+        expect(response.type).toBe('application/json')
+        expect(response.headers['set-cookie']).not.toBeDefined() 
+    })
+    test("should say password is incorect",async()=>{
+        const response = await agent.post("/api/auth/login").send({
+            email: "johnDoe@gmail.com",
+            password: "12345678"
+        })
+        expect(response.status).toBe(400)
+        expect(response.body.message).toBe("Password must contain at least one letter and one number")
+        expect(response.type).toBe('application/json')
+        expect(response.headers['set-cookie']).not.toBeDefined() 
+    })
+    test("should say password is required",async()=>{
+        const response = await agent.post("/api/auth/login").send({
+            email: "johnDoe@gmail.com"
+        })
+        expect(response.status).toBe(400)
+        expect(response.body.message).toBe("Password is required")
+        expect(response.type).toBe('application/json')
+        expect(response.headers['set-cookie']).not.toBeDefined() 
     })
 })
