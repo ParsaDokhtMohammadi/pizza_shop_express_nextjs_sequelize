c561c26aaf4875516082529423c27ce3ca163b1b
parsaDokhtMohammadi <parsadokhtmohammadi.work@gmail.com>
2025-12-15 22:34:29 +0330

    checklogin modified

--- server/common/middlewares/checkLogin.middleware.js
+++ server/common/middlewares/checkLogin.middleware.js
@@ -1,9 +1,9 @@
+import createHttpError from "http-errors"
+
 export const checkLogin = (req, res, next) => {
     try {
         const token = req.signedCookies.planetPizza
-        if(!token){
-            res.status(401).json({message:"وارد حساب کاربری خود شوید"})
-        }
+        if(!token) throw createHttpError(401,"وارد حساب کاربری خود شوید")
         next()
     } catch (err) {
         next(err)
