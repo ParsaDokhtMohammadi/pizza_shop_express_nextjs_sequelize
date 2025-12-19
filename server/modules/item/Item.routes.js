21e183bbfde56a5a407e804dca9624519d12aa3d
parsaDokhtMohammadi <parsadokhtmohammadi.work@gmail.com>
2025-12-19 15:28:15 +0330

    getAll getByid and delete item routes and services added

--- server/modules/item/Item.routes.js
+++ server/modules/item/Item.routes.js
@@ -1,6 +1,6 @@
 import {Router} from "express"
 import { checkAdmin } from "../../common/middlewares/checkAdmin.middleware.js"
-import { createItem, updateItem } from "./Item.service.js"
+import { createItem, deleteItem, getAllItems, getItemById, updateItem } from "./Item.service.js"
 import { uploadFile } from "../../config/multer.config.js"
 import { validate } from "express-validation"
 import { ItemSchema } from "./Item.validation.js"
@@ -8,4 +8,8 @@ const ItemRouter = Router()
 
 ItemRouter.post("",checkAdmin,uploadFile.single("image"),validate(ItemSchema),createItem)
 ItemRouter.post("/:id",checkAdmin,uploadFile.single("image"),validate(ItemSchema),updateItem)
+ItemRouter.delete("/:id",checkAdmin,deleteItem)
+ItemRouter.get("",getAllItems)
+ItemRouter.get("/:id",getItemById)
+
 export {ItemRouter}
--- server/modules/item/Item.service.js
+++ server/modules/item/Item.service.js
@@ -23,28 +23,58 @@ export const createItem = async (req, res, next) => {
     }
 }
 export const updateItem = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const { name, category, description, price } = req.body;
-    const item = await itemModel.findByPk(id);
-    if (!item) {
-      throw createHttpError(404, "محصول یافت نشد");
+    try {
+        const { id } = req.params;
+        const { name, category, description, price } = req.body;
+        const item = await itemModel.findByPk(id);
+        if (!item) {
+            throw createHttpError(404, "محصول یافت نشد");
+        }
+        if (req.file) {
+            item.image_url = `/public/uploads/${req.file.filename}`;
+        }
+
+        item.name = name ?? item.name;
+        item.category = category ?? item.category;
+        item.description = description ?? item.description;
+        item.price = price ?? item.price;
+
+        await item.save();
+
+        res.status(200).json({
+            message: "محصول با موفقیت بروزرسانی شد",
+        });
+    } catch (err) {
+        next(err);
     }
-    if (req.file) {
-      item.image_url = `/public/uploads/${req.file.filename}`;
-    }
-
-    item.name = name ?? item.name;
-    item.category = category ?? item.category;
-    item.description = description ?? item.description;
-    item.price = price ?? item.price;
-
-    await item.save();
-
-    res.status(200).json({
-      message: "محصول با موفقیت بروزرسانی شد",
-    });
-  } catch (err) {
-    next(err);
-  }
 };
+
+export const deleteItem = async (req, res, next) => {
+    try {
+        const { id } = req.params
+        const deletedCount = await itemModel.destroy({where: { id }})
+        if (deletedCount === 0) throw createHttpError(404, "محصول یافت نشد");
+        res.status(200).json({message: "محصول با موفقیت حذف شد"})
+    } catch (err) {
+        next(err)
+    }
+}
+
+export const getAllItems = async(req , res , next)=>{
+    try{
+        const data = await itemModel.findAll()
+        res.status(200).json({message:"محصولات دریافت شدند",data})
+    }catch(err){
+        next(err)
+    }
+}
+export const getItemById = async(req , res , next)=>{
+    try{
+        const {id} = req.params
+        const data = await itemModel.findByPk(id,{raw:true})
+        if(!data)throw createHttpError(404,"محصول یافت نشد")
+        res.status(200).json({message:"محصول دریافت شدند",data})
+    }catch(err){
+        next(err)
+    }
+}
