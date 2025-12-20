782f9e1e1d965eb322e1d8c94b0c9a9fd7ad86f3
parsaDokhtMohammadi <parsadokhtmohammadi.work@gmail.com>
2025-12-20 19:45:53 +0330

    delete and getAll discount route and service added

--- server/modules/discount/Discount.routes.js
+++ server/modules/discount/Discount.routes.js
@@ -2,9 +2,10 @@ import {Router} from  "express"
 import { checkAdmin } from "../../common/middlewares/checkAdmin.middleware.js"
 import { validate } from "express-validation"
 import { DiscountSchema } from "./Discount.validation.js"
-import { AddDiscount } from "./Discount.service.js"
+import { AddDiscount, deleteDiscount, getAllDiscounts } from "./Discount.service.js"
 
 const DiscountRouter = Router()
 DiscountRouter.post("",checkAdmin,validate(DiscountSchema),AddDiscount)
-
+DiscountRouter.get("",checkAdmin,getAllDiscounts)
+DiscountRouter.delete("/:id",checkAdmin,deleteDiscount)
 export {DiscountRouter}
--- server/modules/discount/Discount.service.js
+++ server/modules/discount/Discount.service.js
@@ -25,3 +25,34 @@ export const AddDiscount = async (req, res, next) => {
         next(err)
     }
 }
+export const getAllDiscounts = async (req, res, next) => {
+    try {
+        const discounts = await discountModel.findAll({
+            order: [["start_date", "DESC"]],
+            raw: true
+        });
+
+        res.status(200).json({
+            message: "کدهای تخفیف دریافت شدند",
+            data: discounts
+        });
+    } catch (err) {
+        next(err);
+    }
+};
+export const deleteDiscount = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+
+        if (!id)throw createHttpError(400, "درخواست نامعتبر");
+
+
+        const deletedCount = await discountModel.destroy({where: { id }});
+        if (deletedCount === 0)throw createHttpError(404, "کد تخفیف یافت نشد");
+        res.status(200).json({
+            message: "کد تخفیف با موفقیت حذف شد"
+        });
+    } catch (err) {
+        next(err);
+    }
+};
