6d4f21c409d16d097e6a3fdcb02e46ba1b8ca4a4
parsaDokhtMohammadi <parsadokhtmohammadi.work@gmail.com>
2025-12-20 19:41:24 +0330

    create discount route added

--- server/modules/discount/Discount.service.js
+++ server/modules/discount/Discount.service.js
@@ -2,11 +2,26 @@ import { nanoid } from "nanoid"
 import { discountModel } from "./Discount.model.js"
 import createHttpError from "http-errors"
 
-export const AddDiscount = async(req , res , next)=>{
-    try{
-         const {name , code , percentage , limit , start_date , expiration_date} = req.body
-         
-    }catch(err){
+
+export const AddDiscount = async (req, res, next) => {
+    try {
+        const { name, code, percentage, limit, start_date, expiration_date } = req.body
+        const exists = await discountModel.findOne({ where: { code } })
+        if (exists) throw createHttpError(409, "یک تخفیف دیگر با این کد وجود دارد")
+        await discountModel.create({
+            id: nanoid(6),
+            name,
+            code,
+            percentage,
+            limit,
+            start_date,
+            expiration_date
+        })
+        res.status(200).json({ message: "کد تخفیف ایجاد شد" })
+    } catch (err) {
+        if (err.name === "SequelizeUniqueConstraintError") {
+            return next(createHttpError(409, "یک تخفیف دیگر با این کد وجود دارد"));
+        }
         next(err)
     }
 }
