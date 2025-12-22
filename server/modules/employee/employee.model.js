2c0a6debdf426d8da570222f8b58429f2a494fc6
parsaDokhtMohammadi <parsadokhtmohammadi.work@gmail.com>
2025-12-22 15:41:28 +0330

    employee.model added

---
+++ server/modules/employee/employee.model.js
@@ -0,0 +1,14 @@
+import sequelize from "../../config/sequelize.config.js"
+import { DataTypes } from "sequelize"
+
+export const employeeModel = sequelize.define("employee",{
+    social_code:{type:DataTypes.STRING,primaryKey:true},
+    full_name:{type:DataTypes.STRING,allowNull:false},
+    email:{type:DataTypes.STRING,unique:true,allowNull:false},
+    phone_number:{type:DataTypes.STRING,unique:true,allowNull:false},
+    image_url:{type:DataTypes.STRING,allowNull:false}
+},{
+    freezeTableName:true,
+    createdAt:"created_at",
+    updatedAt:false
+})
