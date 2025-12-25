d839a9ec2239921520c82901a17e38ca3aa7b347
parsaDokhtMohammadi <parsadokhtmohammadi.work@gmail.com>
2025-12-25 15:40:53 +0330

    clustering added

--- server/server.js
+++ server/server.js
@@ -6,31 +6,22 @@ import os from "os";
 
 
 
-//
-// if(cluster.isMaster){
-//     for (let index=0; index<os.cpus().length;index++){
-//         cluster.fork();
-//     }
-// cluster.on("exit", (worker)=>{
-//     console.log(`worker ${worker.process.pid} died restarting`);
-//     cluster.fork()
-// })
-// }else{
-//     app.listen(4000,"0.0.0.0",async ()=>{
-//         try{
-//             await modelsInit()
-//             console.log("server started http://localhost:4000",process.pid)
-//         }catch (err){
-//             console.log(err);
-//         }
-//     })
 
-
-app.listen(4000,"0.0.0.0",async ()=>{
-    try{
-        await modelsInit()
-        console.log("server started http://localhost:4000",process.pid)
-    }catch (err){
-        console.log(err);
+if(cluster.isMaster){
+    for (let index=0; index<os.cpus().length;index++){
+        cluster.fork();
     }
+cluster.on("exit", (worker)=>{
+    console.log(`worker ${worker.process.pid} died restarting`);
+    cluster.fork()
 })
+}else{
+    app.listen(4000,"0.0.0.0",async ()=>{
+        try{
+            await modelsInit()
+            console.log("server started http://localhost:4000",process.pid)
+        }catch (err){
+            console.log(err);
+        }
+    })
+}
